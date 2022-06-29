98a549760d3913867805d0198e50897092496269
Farshad Fakhar <farshadfakhar@gmail.com>
2022-06-29 16:52:58 +0430

    fix

--- Instagram/routes/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use JohnDoe\BlogPackage\Http\Controllers\PostController;
-
-Route::get('/tsd/instagram/callback', [PostController::class, 'index'])->name('posts.index');
